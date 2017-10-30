0b498627fa8ea84c179d7454a0548e9ec6802e8b
ahimsa1 <32558870+ahimsa1@users.noreply.github.com>
2017-10-29 23:18:26 -0400

    Add files via upload
    putting them in the right directory

---
+++ Halite 2/src/hlt/VectorBasic.java
@@ -0,0 +1,54 @@
+package hlt;
+
+public class VectorBasic {
+
+	public double x;
+	public double y;
+	
+	public VectorBasic(Double x1,Double y1) {
+		x = x1;
+		y = y1;
+	}
+	
+	public VectorBasic() {
+		x = 0.0;
+		y = 0.0;
+	}
+	
+	public void polarInputs(Double r,Double angRad) {
+		x = r * Math.cos(angRad);
+		y = r * Math.sin(angRad);
+	}
+	
+	public VectorBasic subtract(VectorBasic v) {
+		return new VectorBasic(x - v.x,y - v.y);
+		
+	}
+	
+	public VectorBasic add(VectorBasic v) {
+		return new VectorBasic(x + v.x,y +v.y) ;
+		
+	}
+	
+	public static VectorBasic add(VectorBasic v1,VectorBasic v2) {
+		return new VectorBasic(v1.x + v2.x,v1.y +v2.y) ;
+		
+	}
+	
+	public static VectorBasic subtract(VectorBasic v1,VectorBasic v2) {
+		return new VectorBasic(v1.x - v2.x,v1.y -v2.y) ;
+		
+	}
+	public Double cross(VectorBasic v) {
+		return x * (v.y) - y * (v.x);
+		
+	}
+	
+	public VectorBasic negate() {
+		return new VectorBasic(-this.x,-this.y);
+		
+	}
+	
+	
+	
+}
