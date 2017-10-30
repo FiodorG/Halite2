e32ed6833fbc6ae36823440cf73f7ef9f7df0f9b
FiodorG <fedor.gorokhovik@gmail.com>
2017-10-29 23:27:26 -0400

    Update the main bots

--- Halite 2/src/MyBot.java
+++ Halite 2/src/MyBot.java
@@ -7,36 +7,35 @@ public class MyBot
 {
     public static void main(final String[] args)
     {
-        final Networking networking = new Networking();
-        Timer timer = new Timer(Config.verbose);
-
+        Timer timer                         = new Timer(Config.verbose);
         ObjectiveManager objectiveManager   = new ObjectiveManager(Config.behaviour);
         FleetManager fleetManager           = new FleetManager();
         NavigationManager navigationManager = new NavigationManager();
         DistanceManager distanceManager     = new DistanceManager();
 
-        final ArrayList<Move> moveList = new ArrayList<>();
-
-        final GameMap gameMap = networking.initialize(Config.botName);
+        final Networking networking         = new Networking();
+        final GameMap gameMap               = networking.initialize(Config.botName);
+        final ArrayList<Move> moveList      = new ArrayList<>();
 
         int turn = 1;
         while(true)
         {
+            logNewTurn(turn++);
             timer.setCurrentTurnStartTime();
 
             moveList.clear();
             gameMap.updateMap(Networking.readLineIntoMetadata());
 
-            distanceManager.computeDistanceMatrix(gameMap);
+            distanceManager.computeDistanceMatrices(gameMap);
             objectiveManager.getObjectives(gameMap, distanceManager);
             fleetManager.assignFleetsToObjectives(gameMap, objectiveManager.getObjectives(), distanceManager);
             navigationManager.moveFleetsToObjective(gameMap, moveList, fleetManager.getFleets());
 
-            // if (timer.timeToEndTurn())
-            //    break;
+            if (timer.timeToEndTurn()) break;
 
-            Networking.sendMoves(moveList);
-            turn++;
+            Networking.sendMoves(navigationManager.resolveMoves(moveList));
         }
     }
+
+    private static void logNewTurn(final int turn)  { DebugLog.addLog("Turn: " + Integer.toString(turn) + "\n"); }
 }
