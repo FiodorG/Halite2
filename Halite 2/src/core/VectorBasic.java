3a4a8ae11d8e25c9fa251252a75b26081485b043
FiodorG <fedor.gorokhovik@gmail.com>
2017-10-30 22:28:14 -0400

    Add new changes on VectorBasic

--- Halite 2/src/core/VectorBasic.java
+++ Halite 2/src/core/VectorBasic.java
@@ -2,16 +2,33 @@ package core;
 
 public class VectorBasic
 {
-    double x;
-    double y;
+    public double x;
+    public double y;
 
-    public VectorBasic(Double x1,Double y1)
+    public VectorBasic(final Double x1, final Double y1)
     {
         x = x1;
         y = y1;
     }
 
-    public VectorBasic subtract(VectorBasic v)  { return new VectorBasic(x - v.x,y - v.y); }
+    public VectorBasic()
+    {
+        x = 0.0;
+        y = 0.0;
+    }
+
+    public void polarInputs(final Double r, final Double angRad)
+    {
+        x = r * Math.cos(angRad);
+        y = r * Math.sin(angRad);
+    }
+
+    public VectorBasic add(final VectorBasic v) { return new VectorBasic(x + v.x,y + v.y); }
+    public VectorBasic subtract(final VectorBasic v) { return new VectorBasic(x - v.x,y - v.y); }
+
+    public static VectorBasic add(final VectorBasic v1, final VectorBasic v2) { return new VectorBasic(v1.x + v2.x,v1.y + v2.y); }
+    public static VectorBasic subtract(final VectorBasic v1, final VectorBasic v2) { return new VectorBasic(v1.x - v2.x,v1.y -v2.y); }
 
     public Double cross(VectorBasic v) { return x * (v.y) - y * (v.x); }
+    public VectorBasic negate() { return new VectorBasic(- this.x, - this.y); }
 }
