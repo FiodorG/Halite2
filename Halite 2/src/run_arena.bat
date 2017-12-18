3fedeb5f0f2b326b0cab48815817e61e1ac092ec
FiodorG <fedor.gorokhovik@gmail.com>
2017-12-18 00:12:32 -0500

    Delete run_arena.bat

--- Halite 2/src/run_arena.bat
+++
@@ -1,8 +0,0 @@
-del /S *.hlt
-del /S *.class
-del /S *.log
-
-javac MyBot.java
-javac MyBotRush.java
-python client/hlt_client/client.py gym -r "java MyBot" -r "java MyBotRush" -b halite -i 50 -H 176 -W 264
-pause
